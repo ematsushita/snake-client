163f4a6ecdd73a6c237d30da6dead6511accb55e
Elissa <elissa.matsushita@gmail.com>
2020-01-15 23:50:32 +0000

    input module

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
-
-
-const handleUserInput = function(stdin) {
-  stdin.on("data", (key) => {
-    if (key === '\u0003') {
-      process.stdout.write("Thanks for playing! \n")
-      process.exit();
-    }
-  })
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput(stdin);
-  return stdin;
-}
 
 
 
